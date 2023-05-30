c4bd7948a0def2c3c94076187ac97a57a08c88ec
Songchai Panich <songchaiza02@gmail.com>
2023-05-30 23:40:56 +0700

    main

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 
-//MongoDB
+// MongoDB Connection
 mongoose.connect('mongodb+srv://admin:1234@cluster0.1piafic.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 
 global.loggedIn = null
 
-//Controllers
+// Controllers
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
@@ -20,9 +20,10 @@ const storeUserController = require('./controllers/storeUserController')
 const loginUserController = require('./controllers/loginUserController')
 const logoutController = require('./controllers/logoutController')
 const homeController = require('./controllers/homeController')
+const aboutController = require('./controllers/aboutController')
+const contactController = require('./controllers/contactController')
 
-const newLocal = './middleware/redirectIfAuth'
-//Middleware
+// Middleware
 const redirectIfAuth = require('./middleware/redirectIfAuth')
 const authMiddleware = require('./middleware/authMiddleware')
 
@@ -31,22 +32,24 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(flash())
 app.use(expressSession({
-    secret: "node scret"
+    secret: "node secret"
 }))
-app.use("*",(req, res, next) => {
+app.use("*", (req, res, next) => {
     loggedIn = req.session.userId
     next()
 })
 app.set('view engine', 'ejs')
 
 app.get('/', indexController)
-app.get('/home', authMiddleware,redirectIfAuth, homeController)
+app.get('/home', authMiddleware, homeController)
 app.get('/login', redirectIfAuth, loginController)
 app.get('/register', redirectIfAuth, registerController)
 app.post('/user/register', redirectIfAuth, storeUserController)
 app.post('/user/login', redirectIfAuth, loginUserController)
 app.get('/logout', logoutController)
+app.get('/about', authMiddleware, aboutController)
+app.get('/contact', authMiddleware, contactController)
 
-app.listen(8000, () => {
-    console.log("App listening on port 8000")
+app.listen(4000, () => {
+    console.log("App listening on port 4000")
 })
